a553f6063702cc06ee7d3ff12d2b74d6db321196
saidamzal <saidamzal@debian-sa.debian-sa>
2025-07-24 02:20:11 +0100

    amediazz

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState} from 'react';
+import LikeButton from './like-button';
  
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
@@ -8,13 +9,7 @@ function Header({ title }) {
  
 export default function HomePage(){
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
- 
-  const [likes, setLikes] = useState(0);
- 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
- 
+
   return (
     <div>
       <Header title="Develop. Preview. Ship." />
@@ -23,8 +18,7 @@ export default function HomePage(){
           <li key={name}>{name}</li>
         ))}
       </ul>
- 
-      <button onClick={handleClick}>Like ({likes})</button>
+    <LikeButton/>
     </div>
   );
 }
